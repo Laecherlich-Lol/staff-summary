d90b5cd2f969369ac423ffbe17fe92ae37c0df92
Yao-Yuan <yao_yuan_@outlook.com>
2020-05-03 15:53:10 +0200

    feat: change to collasible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import xlrd
 import tkinter
+from tkinter import ttk
 import utils
 import create_table
 
@@ -21,17 +22,29 @@ accept_opinions = tkinter.Button(
     command=init.finish
 )
 t1 = tkinter.StringVar()
-t2 = tkinter.StringVar()
+t2 = ttk.Combobox(window, 
+                            values=[
+                                    "January", 
+                                    "February",
+                                    "March",
+                                    "April"])
+
+def callbackFunc(event):
+     print("New Element Selected")
+     print(t2.get())
+
+
+t2.bind("<<ComboboxSelected>>", callbackFunc)
+
 text1 = init.put_in(t1)
-text2 = init.put_in(t2)
 tkinter.Label(window, text='员工:', font=('Arial', 14)).place(x=10, y=30)
 tkinter.Label(window, text='月份:', font=('Arial', 14)).place(x=10, y=60)
 select_file_button.pack()
 text1.pack()
-text2.pack()
+t2.pack()
 accept_opinions.pack()
 window.mainloop()
-data = xlrd.open_workbook(init.print_filename())
+data = xlrd.open_workbook(init.judge())
 table = data.sheets()[int(t2.get())-1]
 new_table = create_table.Create(table, t1.get(), t2.get())
 new_table.init_table()
